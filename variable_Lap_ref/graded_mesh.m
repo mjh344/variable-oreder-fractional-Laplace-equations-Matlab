ba70b86e6aa3dbf1be84e688850b95f93bb58ba9
mjh344 <3443958502@qq.com>
2026-09-03 17:04:29 +0800

    Update graded_mesh.m

--- variable_Lap_ref/graded_mesh.m
+++ variable_Lap_ref/graded_mesh.m
@@ -1,12 +1,5 @@
 
 function x = graded_mesh(a, b, N, alpha_fun)
-% 生成非均匀网格（基于变阶函数 alpha）
-% 输入：a,b 区间端点，N 网格数（内部节点数），alpha_fun 变阶函数句柄
-% 输出：网格点向量（长度 N+1）
-
-    % 计算 alpha 在 [a,b] 上的最大值和最小值
-    % alpha_min = fminbnd(alpha_fun, a, b);
-    % alpha_max = -fminbnd(@(x) -alpha_fun(x), a, b);
 
     x_sample = linspace(a, b, 100001);
     alpha_values = alpha_fun(x_sample);
